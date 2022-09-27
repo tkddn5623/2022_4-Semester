b7eb3d8810ac2713b0c38b4737844bf7e5d48521
tkddn5623 <tkddn5623@g.skku.edu>
2022-09-27 17:48:10 +0900

    5th week :syslab

--- _exercise/_exercise/exercise.c
+++ _exercise/_exercise/exercise.c
@@ -1,19 +1,7 @@
-// #include <unistd.h>
 #include <stdio.h>
-#include <string.h>
 int main() {
-    char buffer[30000];
-    char* args[15000];
-    int argsIdx = 0;
-    while(1) {
-        fgets(buffer, sizeof(buffer), stdin);
-        // printf("[%s]\n", buffer);
-        *(strrchr(buffer, '\n')) = '\0';
-        args[argsIdx++] = strtok(buffer, " ");
-        while(strtok(NULL, " ")){
-            
-        }
-        
-    }
+    int a;
+    scanf("%d", &a);
+    printf("%d\n", a);
     
 }
--- systemProgrammingLab5/Syslab5.c
+++ systemProgrammingLab5/Syslab5.c
@@ -1,38 +1,30 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <stdio.h>
 #include <string.h>
+void runShell(char* const* args) {
+    char path[1000] = "/bin/";
+    strcat(path, args[0]);
+    if (fork() == 0) {
+        execv(path, args);
+        _exit(0);
+    }
+    else {
+        wait(NULL);
+    }
+}
 int main() {
     char buffer[30000];
     char* args[15000];
     int argsSize;
-    pid_t pid = -1;
-    int num = 1111;
     while (1) {
-        if(pid != -1) printf("pid [%d][num:%d] waiting input.\n", pid, num);
-        fgets(buffer, sizeof(buffer), stdin);
-        if(pid != -1) printf("pid [%d][num:%d] complete input.\n", pid, num);
-        *(strrchr(buffer, '\n')) = '\0';
+        read(STDIN_FILENO, buffer, sizeof(buffer));
+        *(strchr(buffer, '\n')) = '\0';
         args[(argsSize = 0)] = strtok(buffer, " ");
-        while ((args[++argsSize] = strtok(NULL, " ")) != NULL); 
-        if(!strcmp(args[0], "exit")){
-            printf("Exiting, pid : %d.\n", pid);
-            break;
-        }
-        if(pid != -1) printf("pid [%d] starting fork...\n", pid);
-        if ((pid = fork()) == 0) {
-            printf("hello from child\n");
-            num += 1;
-        } 
-        else {
-            printf("hello from parent[%d]\n", pid);
-            // wait(NULL);
-            num += 100;
-        }
-        printf("my pid is %d, and the num is %d\n", pid, num);
-        
+        if (args[0] == NULL) continue;
+        while ((args[++argsSize] = strtok(NULL, " ")) != NULL);
 
-
+        if (!strcmp(args[0], "quit")) break;
+        else runShell(args);
     }
 }
