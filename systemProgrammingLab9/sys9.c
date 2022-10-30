de97ba99837b4f63e311acde3d68c0cd7830e91c
tkddn5623 <tkddn5623@g.skku.edu>
2022-10-31 00:00:41 +0900

    week9 :sys9 making

---
+++ systemProgrammingLab9/sys9.c
@@ -0,0 +1,72 @@
+#include <unistd.h>
+#include <sys/msg.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+#define MAXBUF (1024)
+
+typedef struct _msgbuf_t {
+    long msgtype;
+    char text[MAXBUF];
+} msgbuf_t;
+
+char* myitos(char* dest, const int num) {
+    int digitsSize = 0;
+    int digits[10];
+    for (int i = num; i > 0; i /= 10) {
+        digits[digitsSize++] = i % 10;
+    }
+    for (int i = digitsSize - 1; i >= 0; i--) {
+        dest[digitsSize - 1 - i] = digits[i] + '0';
+    }
+    dest[digitsSize] = '\0';
+    return dest;
+}
+int runchat(const key_t key1, const key_t key2) {
+    const int qid[2] = { msgget(key1, IPC_CREAT | 0666), msgget(key2, IPC_CREAT | 0666) };
+    char buffer[MAXBUF] = { 0 };
+    msgbuf_t msgbuf_r;
+    msgbuf_t msgbuf_w;
+    if (qid[0] < 0 || qid[1] < 0) return -1;
+    if (fork() == 0) {
+        while (1) {
+            time_t t;
+            memset(buffer, 0, sizeof(buffer));
+            msgrcv(qid[0], &msgbuf_r, sizeof(msgbuf_t), 0, 0);
+            time(&t);
+            printf("%s\n", msgbuf_r.text);
+            
+            myitos(buffer, key1);
+            strcat(buffer, " read message at:\t");
+            strcat(buffer, ctime(&t));
+            strcpy(msgbuf_w.text, buffer);
+            msgbuf_w.msgtype = 1;
+            printf("%s\n", msgbuf_w.text);
+            
+
+
+        }
+    }
+    else {
+        while (1) {
+            memset(buffer, 0, sizeof(buffer));
+            scanf("%s", buffer);
+            strcpy(msgbuf_w.text, "RECEIVED ");
+            strcat(msgbuf_w.text, buffer);
+            msgbuf_w.msgtype = 1;
+            msgsnd(qid[1], &msgbuf_w, sizeof(msgbuf_t), 0);
+        }
+    }
+    return 0;
+}
+int main() {
+    int temp;
+    key_t key[2];
+    printf("my id is\n");
+    scanf("%d", &key[0]);
+    // key[0] = ftok(".", temp);
+    printf("who to send ?\n");
+    scanf("%d", &key[1]);
+    // key[1] = ftok(".", temp);
+    runchat(key[0], key[1]);
+}
