f6cb961cdd23b2fcad7e7c75ef1d5ae2d2bac8e6
tkddn5623 <tkddn5623@g.skku.edu>
2022-11-02 21:20:07 +0900

    10주차 중반

--- _exercise/_exercise/exercise.c
+++ _exercise/_exercise/exercise.c
@@ -1,31 +1,19 @@
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <string.h>
-void runShell(char* const* args) {
-    char path[1000] = "/bin/";
-    strcat(path, args[0]);
-    if (fork() == 0) {
-        execv(path, args);
-        _exit(0);
-    }
-    else {
-        wait(NULL);
-    }
-    int* args2;
-}
+#include <stdio.h>
 int main() {
-    char buffer[30000];
-    char* args[15000];
-    int argsSize;
-    while (1) {
-        read(STDIN_FILENO, buffer, sizeof(buffer));
-        *(strchr(buffer, '\n')) = '\0';
-        args[(argsSize = 0)] = strtok(buffer, " ");
-        if (args[0] == NULL) continue;
-        while ((args[++argsSize] = strtok(NULL, " ")) != NULL);
-
-        if (!strcmp(args[0], "quit")) break;
-        else runShell(args);
-    }
+	{
+		//freopen("i.txt", "r", stdin);
+	}
+	char C[100];
+	int N, X, drink = 0;
+	scanf("%d%d", &N, &X);
+	for (int i = 0; i < N; i++) {
+		scanf(" %c", &C[i]);
+	}
+	for (int i = 0; i < N; i++) {
+		printf("%c %d ", C[i], i);
+		if ((C[i] - X - 1) / N < (C[drink] - X - 1) / N) {
+			drink = i;
+		}
+	}
+	printf("%d", drink + 1);
 }
